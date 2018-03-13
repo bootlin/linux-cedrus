365d0c5bbd7dc80658f59c350d1f469a9f3ce777
Paul Kocialkowski <paul.kocialkowski@bootlin.com>
2018-03-13 09:13:01 +0100

    drm/sun4i: Disable frontend video channel before enabling a layer
    This adds a dedicated function for disabling the layer video channel
    from the frontend to the backend. It is called automatically on an
    atomic update, as to disable the frontend by default (it will be enabled
    later on in the atomic update if necessary).
    
    It fixes an issue when enabling a layer that uses the frontend (e.g. for
    scaling) and then reusing the same layer without the frontend. Since the
    video channel to the frontend was never disabled, the backend was still
    trying to get data from there.
    
    Signed-off-by: Paul Kocialkowski <paul.kocialkowski@bootlin.com>

--- drivers/gpu/drm/sun4i/sun4i_backend.h
+++ drivers/gpu/drm/sun4i/sun4i_backend.h
@@ -207,5 +207,7 @@ int sun4i_backend_update_layer_frontend(struct sun4i_backend *backend,
 					int layer, uint32_t in_fmt);
 int sun4i_backend_update_layer_zpos(struct sun4i_backend *backend,
 				    int layer, struct drm_plane *plane);
+void sun4i_backend_disable_layer_frontend(struct sun4i_backend *backend,
+					  int layer);
 
 #endif /* _SUN4I_BACKEND_H_ */
