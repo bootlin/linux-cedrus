ebd2bca34f70c9e46e7622732d0709426829871f
Hans Verkuil <hans.verkuil@cisco.com>
2018-05-21 11:54:27 +0300

    media-request: implement media requests
    Add initial media request support:
    
    1) Add MEDIA_IOC_REQUEST_ALLOC ioctl support to media-device.c
    2) Add struct media_request to store request objects.
    3) Add struct media_request_object to represent a request object.
    4) Add MEDIA_REQUEST_IOC_QUEUE/REINIT ioctl support.
    
    Basic lifecycle: the application allocates a request, adds
    objects to it, queues the request, polls until it is completed
    and can then read the final values of the objects at the time
    of completion. When it closes the file descriptor the request
    memory will be freed (actually, when the last user of that request
    releases the request).
    
    Drivers will bind an object to a request (the 'adds objects to it'
    phase), when MEDIA_REQUEST_IOC_QUEUE is called the request is
    validated (req_validate op), then queued (the req_queue op).
    
    When done with an object it can either be unbound from the request
    (e.g. when the driver has finished with a vb2 buffer) or marked as
    completed (e.g. for controls associated with a buffer). When all
    objects in the request are completed (or unbound), then the request
    fd will signal an exception (poll).
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Co-developed-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Co-developed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Co-developed-by: Alexandre Courbot <acourbot@chromium.org>

--- drivers/media/media-device.c
+++ drivers/media/media-device.c
@@ -32,6 +32,7 @@
 #include <media/media-device.h>
 #include <media/media-devnode.h>
 #include <media/media-entity.h>
+#include <media/media-request.h>
 
 #ifdef CONFIG_MEDIA_CONTROLLER
 
@@ -366,6 +367,15 @@ static long media_device_get_topology(struct media_device *mdev,
 	return ret;
 }
 
+static long media_device_request_alloc(struct media_device *mdev,
+				       struct media_request_alloc *alloc)
+{
+	if (!mdev->ops || !mdev->ops->req_validate || !mdev->ops->req_queue)
+		return -ENOTTY;
+
+	return media_request_alloc(mdev, alloc);
+}
+
 static long copy_arg_from_user(void *karg, void __user *uarg, unsigned int cmd)
 {
 	/* All media IOCTLs are _IOWR() */
@@ -414,6 +424,7 @@ static const struct media_ioctl_info ioctl_info[] = {
 	MEDIA_IOC(ENUM_LINKS, media_device_enum_links, MEDIA_IOC_FL_GRAPH_MUTEX),
 	MEDIA_IOC(SETUP_LINK, media_device_setup_link, MEDIA_IOC_FL_GRAPH_MUTEX),
 	MEDIA_IOC(G_TOPOLOGY, media_device_get_topology, MEDIA_IOC_FL_GRAPH_MUTEX),
+	MEDIA_IOC(REQUEST_ALLOC, media_device_request_alloc, 0),
 };
 
 static long media_device_ioctl(struct file *filp, unsigned int cmd,
@@ -686,6 +697,8 @@ void media_device_init(struct media_device *mdev)
 	INIT_LIST_HEAD(&mdev->pads);
 	INIT_LIST_HEAD(&mdev->links);
 	INIT_LIST_HEAD(&mdev->entity_notify);
+
+	mutex_init(&mdev->req_queue_mutex);
 	mutex_init(&mdev->graph_mutex);
 	ida_init(&mdev->entity_internal_idx);
 
@@ -699,6 +712,7 @@ void media_device_cleanup(struct media_device *mdev)
 	mdev->entity_internal_idx_max = 0;
 	media_graph_walk_cleanup(&mdev->pm_count_walk);
 	mutex_destroy(&mdev->graph_mutex);
+	mutex_destroy(&mdev->req_queue_mutex);
 }
 EXPORT_SYMBOL_GPL(media_device_cleanup);
 
