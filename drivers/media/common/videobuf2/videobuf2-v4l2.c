b3371ee296c310a33bf2c54d35fe69b96929e666
Hans Verkuil <hans.verkuil@cisco.com>
2018-05-21 11:54:52 +0300

    videobuf2-v4l2: add vb2_request_queue/validate helpers
    The generic vb2_request_validate helper function checks if
    there are buffers in the request and if so, prepares (validates)
    all objects in the request.
    
    The generic vb2_request_queue helper function queues all buffer
    objects in the validated request.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>

--- drivers/media/common/videobuf2/videobuf2-v4l2.c
+++ drivers/media/common/videobuf2/videobuf2-v4l2.c
@@ -1100,6 +1100,44 @@ void vb2_ops_wait_finish(struct vb2_queue *vq)
 }
 EXPORT_SYMBOL_GPL(vb2_ops_wait_finish);
 
+int vb2_request_validate(struct media_request *req)
+{
+	struct media_request_object *obj;
+	int ret = 0;
+
+	if (!vb2_request_has_buffers(req))
+		return -ENOENT;
+
+	list_for_each_entry(obj, &req->objects, list) {
+		if (!obj->ops->prepare)
+			continue;
+
+		ret = obj->ops->prepare(obj);
+		if (ret)
+			break;
+	}
+
+	if (ret) {
+		list_for_each_entry_continue_reverse(obj, &req->objects, list)
+			if (obj->ops->unprepare)
+				obj->ops->unprepare(obj);
+		return ret;
+	}
+	return 0;
+}
+EXPORT_SYMBOL_GPL(vb2_request_validate);
+
+void vb2_request_queue(struct media_request *req)
+{
+	struct media_request_object *obj;
+
+	list_for_each_entry(obj, &req->objects, list) {
+		if (obj->ops->queue)
+			obj->ops->queue(obj);
+	}
+}
+EXPORT_SYMBOL_GPL(vb2_request_queue);
+
 MODULE_DESCRIPTION("Driver helper framework for Video for Linux 2");
 MODULE_AUTHOR("Pawel Osciak <pawel@osciak.com>, Marek Szyprowski");
 MODULE_LICENSE("GPL");
