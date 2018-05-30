bcb528567cb5fd6a882e37b33612e8d680907909
Hans Verkuil <hans.verkuil@cisco.com>
2018-05-30 08:46:22 +0200

    videobuf2-v4l2: integrate with media requests
    This implements the V4L2 part of the request support. The main
    change is that vb2_qbuf and vb2_prepare_buf now have a new
    media_device pointer. This required changes to several drivers
    that did not use the vb2_ioctl_qbuf/prepare_buf helper functions.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>

--- drivers/staging/media/davinci_vpfe/vpfe_video.c
+++ drivers/staging/media/davinci_vpfe/vpfe_video.c
@@ -1422,7 +1422,8 @@ static int vpfe_qbuf(struct file *file, void *priv,
 		return -EACCES;
 	}
 
-	return vb2_qbuf(&video->buffer_queue, p);
+	return vb2_qbuf(&video->buffer_queue,
+			video->video_dev.v4l2_dev->mdev, p);
 }
 
 /*
